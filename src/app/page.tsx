adc3ea679033203383841a5b4f0c816991842c71
Emmanuel Ogbizi-Ugbe <iamogbz@gmail.com>
2025-08-22 21:27:40 +0000

    Keep the "Lexicle The classic word game, reimagined." and "Challenge you

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+
 import { Logo } from "@/components/logo";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
