10fe639b0d5841fc1f5ddc7b377355dc28483a7f
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2025-09-11 10:33:37 -0400

    Fix prevent auto zoom into page when inputs are seleced

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, minimum-scale=1, maximum-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
